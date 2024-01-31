9f6080e4316fc37b36da89cfb4d8187e06f74413
LAmerkamp <lukas.amerkamp@web.de>
2024-01-31 17:41:20 +0100

    add load annimation

--- src/popup/components/Overlay/ProcessingOverlay.tsx
+++ src/popup/components/Overlay/ProcessingOverlay.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LicciumIconSvg } from './LicciumIconSvg';
+import { LoadCycleSvg } from './LoadCycleSvg';
 import './ProcessingOverlay.css';
 
 export const ProcessingOverlay = () => {
@@ -8,7 +8,19 @@ export const ProcessingOverlay = () => {
         <div className="ProcessingOverlay">
             <div className="processingOverlayloading">
                 <div className="processingOverlaylicciumIcon">
-                    <LicciumIconSvg />
+                    {/* <LoadCycleSvg /> */}
+                    <section>
+                        <div className="circle0 circle"></div>
+                        <div className="circle1 circle"></div>
+                        <div className="circle2 circle"></div>
+                        <div className="circle3 circle"></div>
+                        <div className="circle4 circle"></div>
+                        <div className="circle5 circle"></div>
+                        <div className="circle6 circle"></div>
+                        <div className="circle7 circle"></div>
+                        <div className="circle8 circle"></div>
+                        <div className="circle9 circle"></div>
+                    </section>
                 </div>
                 <p className="processingOverlayloadingText">Generating ISCC,<br />searching for declarations.</p>
             </div>
