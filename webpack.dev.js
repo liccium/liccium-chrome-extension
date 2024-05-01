0bf67702e189401c2dda3c76fac1077ed2ea8ea3
Roy <roy@osherove.com>
2024-05-01 11:39:41 +0300

    Add .env to load api url in dev V2-7

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,19 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const EnvironmentPlugin = require('webpack').EnvironmentPlugin;
+// dotenv
+
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'cheap-module-source-map',
+    plugins:[
+        new EnvironmentPlugin({
+            API_URL: process.env.API_URL,
+            API_KEY: process.env.API_KEY,
+        })
+    ]
 });
+
